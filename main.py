d8e4ce33a001ae935e57eb6e512bd8bba21ce28d
Amaury Chronowski <amaury.chronowski@cpe.fr>
2022-06-07 09:12:18 +0200

    suite

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from zlib import Z_NO_COMPRESSION
 from viewerGL import ViewerGL
 import glutils
 from mesh import Mesh
@@ -37,27 +38,27 @@ def main():
 
     m = Mesh.load_obj('cube.obj')
     m.normalize()
-    m.apply_matrix(pyrr.matrix44.create_from_scale([2, 5, 2, 1]))
+    m.apply_matrix(pyrr.matrix44.create_from_scale([2, 2.5, 2, 1]))
     n = Mesh.load_obj('stegosaurus.obj')
     n.normalize()
     n.apply_matrix(pyrr.matrix44.create_from_scale([2, 2, 2, 1]))
+    texturew = glutils.load_texture('Wall.jpg')
+    textures = glutils.load_texture('stegosaurus.jpg')
     for ligne in map:
+        # print('a')
         for bloc in ligne:
+            # print('b')
             if bloc==1:
-                tr = Transformation3D(euler=pyrr.euler.create(), center=pyrr.Vector3(), translation=pyrr.Vector3([xcord,0,zcord]))
-                tr.translation.z = 0
-                tr.rotation_center.z = 0.2
-                texture = glutils.load_texture('Wall.jpg')
-                o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
+                tr = Transformation3D(euler=pyrr.euler.create(), center=pyrr.Vector3(), translation=pyrr.Vector3([xcord,2.5,zcord]))
+                o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texturew, tr)
                 viewer.add_object(o)
             if bloc==2:
                 tr = Transformation3D(euler=pyrr.euler.create(), center=pyrr.Vector3(), translation=pyrr.Vector3([xcord,0,zcord]))
-                tr.translation.z = 0
-                tr.rotation_center.z = 0.2
-                texture = glutils.load_texture('stegosaurus.jpg')
-                o = Object3D(n.load_to_gpu(), n.get_nb_triangles(), program3d_id, texture, tr)
+                o = Object3D(n.load_to_gpu(), n.get_nb_triangles(), program3d_id, textures, tr)
                 viewer.add_object(o)
+            print(xcord, zcord)
             xcord+=2
+        xcord = -32
         zcord+=2         
 
     m = Mesh()
